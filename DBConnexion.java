1f69985a7795b08f4740a9c64975b491c2375c05
avissejoel5-web <avissejoel5@gmail.com>
2026-01-16 21:00:17 +0000

    Add files via upload

---
+++ DBConnexion.java
@@ -0,0 +1,40 @@
+package org.example.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnexion {
+
+    private static final String url = "jdbc:mysql://localhost:3306/personnes";
+    private static final String user = "root";
+    private static final String password = "";
+
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection established");
+            return conn;
+        } catch (SQLException ex) {
+            System.out.println("Erreur de connexion");
+            ex.printStackTrace();
+            return null;  // Obligatoire ici pour que la méthode retourne toujours une valeur
+        }
+    }
+
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+                System.out.println("Connection closed");
+            } catch (SQLException ex) {
+                System.out.println("Erreur lors de la fermeture de la connexion");
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+}
